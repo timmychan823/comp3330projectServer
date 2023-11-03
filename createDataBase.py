544d0924f738c94db48ea50ec98f3b317793a31b
timmychan823 <chanshunhei09@gmail.com>
2023-11-03 22:42:56 +0800

    Created server

---
+++ createDataBase.py
@@ -0,0 +1,71 @@
+import sqlite3
+# Connects to database
+# The .db file is created automatically if it does not exist 
+con = sqlite3.connect('my-db.db')
+
+# Creates table
+con.execute("""CREATE TABLE IF NOT EXISTS User (
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    display_name TEXT NOT NULL,
+    email_address TEXT NOT NULL,
+    password TEXT NOT NULL,
+    year INTEGER NOT NULL,
+    profile_pic INTEGER
+);""")
+
+con.execute("""CREATE TABLE IF NOT EXISTS Hobbies (
+    user_id INTEGER NOT NULL,
+    hobby TEXT NOT NULL
+);""")
+
+con.execute("""CREATE TABLE IF NOT EXISTS Majors (
+    user_id INTEGER NOT NULL,
+    major VARCHAR NOT NULL
+);""")
+
+con.execute("""CREATE TABLE IF NOT EXISTS ChatRoom (
+    chatroom_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    owner1_id INTEGER NOT NULL,
+	owner2_id INTEGER NOT NULL
+);""")
+
+con.execute("""CREATE TABLE IF NOT EXISTS ChatRoom (
+    chatroom_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    owner1_id INTEGER NOT NULL,
+	owner2_id INTEGER NOT NULL
+);""")
+
+con.execute("""CREATE TABLE IF NOT EXISTS Message (
+    chatroom_id INTEGER NOT NULL,
+	sent_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	message_content TEXT NOT NULL,
+    sender_id INTEGER NOT NULL
+);""")
+
+con.execute("""CREATE TABLE IF NOT EXISTS Match (
+    initiator_id INTEGER NOT NULL,
+	restaurant_name TEXT NOT NULL,
+	proposed_timeslot_of_match TEXT NOT NULL
+);""")
+
+
+con.execute("""CREATE TABLE IF NOT EXISTS Restaurant (
+    restaurant_name TEXT NOT NULL,
+	restaurant_location TEXT NOT NULL
+);""")
+
+con.execute("""CREATE TABLE IF NOT EXISTS Timeslots (
+    restaurant_name TEXT NOT NULL,
+	timeslot TEXT NOT NULL
+);""")
+
+# insert test data
+testData = [["Timmy","cshtimmy@connect.hku.hk","123456",3]]
+for user in testData:
+    print(user)
+    insertQuery = f"INSERT INTO User (display_name,email_address,password,year) values (\"{user[0]}\",\"{user[1]}\",\"{user[2]}\",{user[3]});" 
+    print(insertQuery)
+    con.execute(insertQuery)
+con.commit()
+con.close()
+
---
+++ server.py
@@ -0,0 +1,46 @@
+import flask
+import sqlite3
+from flask import request
+
+app = flask.Flask(__name__)
+
+@app.route('/login', methods=['GET']) 
+def login():
+    email_address = request.args.get('email_address') 
+    password = request.args.get('password') 
+
+    print(email_address)
+    print(password)
+
+    con = sqlite3.connect('my-db.db')
+
+    """if len(restaurant_name) > 0 and len(restaurant_location)>0:
+        # insert into db
+        insertQuery = f"INSERT INTO Restaurant (restaurant_name,restaurant_location) values (\"{restaurant_name}\",\"{restaurant_location}\");"
+        con.execute(insertQuery)
+        con.commit()"""
+
+    # select from db
+    cursor = con.execute(f"SELECT email_address, password FROM User WHERE email_address=\"{email_address}\" AND password=\"{password}\";")
+    userLoginData = []
+
+    is_there_something_returned = False
+    for row in cursor:
+        is_there_something_returned = True
+        userLoginData.append([row[0],row[1]]) 
+    if not is_there_something_returned:
+        userLoginData.append(["Not in database","Not in database"])
+
+
+    con.close()
+    outdata = {
+  
+        "returned_email_address": userLoginData[0][0],
+        "returned_password": userLoginData[0][1]
+    }
+
+    print(outdata)
+
+    return outdata
+# adds host="0.0.0.0" to make the server publicly available 
+app.run(host="0.0.0.0",port=8964)
